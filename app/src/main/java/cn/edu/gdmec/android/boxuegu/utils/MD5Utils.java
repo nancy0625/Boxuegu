3c3a8f2a3c8af2e1dd422f6e44995944d4272a3a
nancy0625 <1308013168@qq.com>
2018-03-08 09:36:20 +0800

    3-5task

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/utils/MD5Utils.java
@@ -0,0 +1,38 @@
+package cn.edu.gdmec.android.boxuegu.utils;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Created by asus on 2018/3/8.
+ */
+
+public class MD5Utils {
+    /**
+     * MD5加密算法
+     *
+     */
+    public static String md5(String text){
+        MessageDigest digest = null;
+        try {
+            digest = MessageDigest.getInstance("md5");
+            byte[] result = digest.digest(text.getBytes());
+            StringBuilder sb = new StringBuilder();
+            for (byte b : result){
+                int number = b & 0xff;
+                String hex = Integer.toHexString(number);
+                if (hex.length() == 1){
+                    sb.append("0"+hex);
+
+                }else {
+                    sb.append(hex);
+                }
+            }
+            return sb.toString();
+
+        }catch (NoSuchAlgorithmException e){
+            e.printStackTrace();
+            return "";
+        }
+    }
+}
