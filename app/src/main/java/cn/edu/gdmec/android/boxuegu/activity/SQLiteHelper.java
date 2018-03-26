f054edca213d8e39be5a3724a2d30832049c3f35
nancy0625 <1308013168@qq.com>
2018-03-26 19:15:52 +0800

    5-3  finished

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/SQLiteHelper.java
@@ -0,0 +1,39 @@
+package cn.edu.gdmec.android.boxuegu.activity;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by asus on 2018/3/26.
+ */
+
+public class SQLiteHelper  extends SQLiteOpenHelper{
+    private static final int DB_VERSION = 1;
+    public static String DB_NAME = "bxg.db";
+    public static final String U_USERINFO = "userinfo";
+
+
+
+    public SQLiteHelper(Context context) {
+        super(context, DB_NAME , null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS " + U_USERINFO + "(" +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                "userame VARCHAR, " +
+                "nickName VARCHAR, " +
+                "sex VARCHAR, " +
+                "signature VARCHAR" +
+                ")");
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+ U_USERINFO);
+        onCreate(db);
+    }
+}
