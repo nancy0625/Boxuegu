12706b94bee2b8ebe5c749bfc289bcf789ecd811
nancy0625 <1308013168@qq.com>
2018-03-26 19:35:42 +0800

    3- 10  finished

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/UserInfoActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/UserInfoActivity.java
@@ -2,14 +2,28 @@ package cn.edu.gdmec.android.boxuegu.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import cn.edu.gdmec.android.boxuegu.R;
 
-public class UserInfoActivity extends AppCompatActivity {
+public class UserInfoActivity extends AppCompatActivity implements View.OnClickListener{
+    private TextView tv_back;
+    private TextView tv_main_title;
+    private TextView tv_nickName,tv_signature,tv_user_name,tv_sex;
+    private RelativeLayout rl_nickName,rl_sex,rl_signature,rl_title_bar;
+    private String spUserName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
