1ba62b5165139c46d9459ed6d4701f7bdbf0eeba
nancy0625 <1308013168@qq.com>
2018-03-26 19:04:27 +0800

    5-2  finished

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/UserBean.java
@@ -0,0 +1,12 @@
+package cn.edu.gdmec.android.boxuegu.activity;
+
+/**
+ * Created by asus on 2018/3/26.
+ */
+
+public class UserBean {
+    public String userName;
+    public String nickName;
+    public String sex;
+    public String signature;
+}
