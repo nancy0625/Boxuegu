8e29cee9f903e3fc8304bf94630b1451bb05350e
nancy0625 <1308013168@qq.com>
2018-04-02 09:27:10 +0800

    7-7 update  waitting

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/Fragment/AdBannerFragment.java
@@ -0,0 +1,10 @@
+package cn.edu.gdmec.android.boxuegu.Fragment;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Created by asus on 2018/4/2.
+ */
+
+public class AdBannerFragment extends Fragment {
+}
