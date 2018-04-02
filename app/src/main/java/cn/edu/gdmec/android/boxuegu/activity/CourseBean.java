041be01174598ed514a995e579e2123e3b569acb
nancy0625 <1308013168@qq.com>
2018-04-02 09:25:05 +0800

    7-7 update  waitting

---
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/CourseBean.java
@@ -0,0 +1,14 @@
+package cn.edu.gdmec.android.boxuegu.activity;
+
+/**
+ * Created by asus on 2018/4/2.
+ */
+
+public class CourseBean {
+    public int id; //每章id
+    public String imgTitle;//图片上的标题
+    public String title;//章标题
+    public String intro;//章视频简介
+    public String icon;//广告栏上的图片
+
+}
