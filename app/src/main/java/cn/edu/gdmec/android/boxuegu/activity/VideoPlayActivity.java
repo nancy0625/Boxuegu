dfa894e9da68b9753706f2b253bb9684595f2c66
nancy0625 <1308013168@qq.com>
2018-04-07 22:29:55 +0800

    7.3 action

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/VideoPlayActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/VideoPlayActivity.java
@@ -1,15 +1,19 @@
 package cn.edu.gdmec.android.boxuegu.activity;
 
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.KeyEvent;
 import android.view.WindowManager;
 import android.widget.MediaController;
 import android.widget.Toast;
 import android.widget.VideoView;
 
+import cn.edu.gdmec.android.boxuegu.MainActivity;
 import cn.edu.gdmec.android.boxuegu.R;
+import cn.edu.gdmec.android.boxuegu.utils.AnalysisUtils;
 
 public class VideoPlayActivity extends AppCompatActivity {
     private VideoView videoView;
@@ -46,5 +50,17 @@ public class VideoPlayActivity extends AppCompatActivity {
         String uri = "android.resource://"+getPackageName() + "/" + R.raw.video11;
         videoView.setVideoPath(uri);
         videoView.start();
+    }
+    /**
+     * 点击后退按钮
+     */
+    protected  long exitTime;
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        //把视频详情界面传递过来的被点击视频的位置传递回去
+        Intent data = new Intent();
+        data.putExtra("position",position);
+        setResult(RESULT_OK,data);
+        return super.onKeyDown(keyCode,event);
     }
 }
