2ae878a426b35c29eb0c9bca30f022126cf437d6
nancy0625 <1308013168@qq.com>
2018-05-02 01:04:20 +0800

    updated 5.08 加权限

--- app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/VideoPlayActivity.java
+++ app/src/main/java/cn/edu/gdmec/android/boxuegu/activity/VideoPlayActivity.java
@@ -2,9 +2,11 @@ package cn.edu.gdmec.android.boxuegu.activity;
 
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.WindowManager;
 import android.widget.MediaController;
@@ -47,8 +49,11 @@ public class VideoPlayActivity extends AppCompatActivity {
             Toast.makeText(this,"本地没有视频，暂时无法播放",Toast.LENGTH_SHORT).show();
             return;
         }
-
-        videoView.setVideoPath(videoPath);
+       // String uri = "android.resource://"+getPackageName() + "/" + R.raw.video11;
+        Log.i("Video",videoPath);
+        String uri = videoPath;
+        Uri uri2 = Uri.parse(uri);
+        videoView.setVideoURI(uri2);
         videoView.start();
     }
     /**
